01b634d4b1d01c343afee4895a7cdef8acbadade
Mathushuthanan Chakravarthy S <144238094+mathushuthanans@users.noreply.github.com>
2025-07-26 16:17:33 +0530

    Delete backend-adci2 directory

--- backend-adci2/app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.api.routes import main_routes
-
-app = FastAPI()
-
-app.include_router(main_routes.router)
--- backend-adci2/setup.py
+++
@@ -1,3 +0,0 @@
-# setup.py
-from setuptools import setup, find_packages
-setup(name="backend_prototype", packages=find_packages())
